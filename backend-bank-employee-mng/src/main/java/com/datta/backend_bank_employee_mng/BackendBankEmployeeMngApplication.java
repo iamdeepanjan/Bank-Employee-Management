feb2d88b9223f31c30a56256b32ef5a7a154b7ed
iamdeepanjan <deepanjanbmp3@gmail.com>
2024-11-10 12:19:08 +0530

    Initial Commit

---
+++ backend-bank-employee-mng/src/main/java/com/datta/backend_bank_employee_mng/BackendBankEmployeeMngApplication.java
@@ -0,0 +1,13 @@
+package com.datta.backend_bank_employee_mng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBankEmployeeMngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBankEmployeeMngApplication.class, args);
+	}
+
+}
---
+++ backend-bank-employee-mng/src/test/java/com/datta/backend_bank_employee_mng/BackendBankEmployeeMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datta.backend_bank_employee_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBankEmployeeMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
