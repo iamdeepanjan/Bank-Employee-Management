d7eb10e535e0cdd4dcfc7a8d3bb0f2174766263e
DEEPANJAN DATTA <90570726+iamdeepanjan@users.noreply.github.com>
2024-11-21 11:24:54 +0000

    user full done

--- frontend-bank-employee-mng/src/app/user/update-user-details/update-user-details.component.ts
+++ frontend-bank-employee-mng/src/app/user/update-user-details/update-user-details.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Employee } from '../employee';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user-details',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './update-user-details.component.html',
   styleUrl: './update-user-details.component.css'
 })
-export class UpdateUserDetailsComponent {
+export class UpdateUserDetailsComponent implements OnInit {
 
+  updateForm: FormGroup = new FormGroup({});
+  employee: Employee = new Employee();
+  successMessage: string = "";
+
+  private userService = inject(UserService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.userService.getUserDetails().subscribe(data => {
+      this.employee = data;
+      this.updateForm.patchValue({
+        name: this.employee.name,
+        mobileNo: this.employee.mobileNo,
+        address: this.employee.address
+      });
+    });
+
+    this.updateForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      mobileNo: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      address: ['', Validators.required]
+    })
+  }
+
+  onSubmit():void{
+    if(this.updateForm.valid){
+      const updatedEmployee:Employee = this.updateForm.value;
+      this.userService.updateDetails(updatedEmployee).subscribe({
+        next: (response: any) => {
+          this.successMessage = response.message;
+          setTimeout(() => {
+            this.router.navigate(['/user/user-details']);
+          }, 2000);
+        },
+        error: (error: any) => {
+          console.log(error);
+        }
+      });
+    }
+    else{
+      this.makeFormDirty(this.updateForm);
+    }
+  }
+
+  makeFormDirty(form:FormGroup){
+    Object.keys(form.controls).forEach(key => {
+      form.controls[key].markAsDirty();
+      form.controls[key].markAsTouched();
+    });
+  }
 }
--- frontend-bank-employee-mng/src/app/user/update-user-password/update-user-password.component.ts
+++ frontend-bank-employee-mng/src/app/user/update-user-password/update-user-password.component.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user-password',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './update-user-password.component.html',
   styleUrl: './update-user-password.component.css'
 })
-export class UpdateUserPasswordComponent {
+export class UpdateUserPasswordComponent implements OnInit, OnDestroy{
 
+  passwordForm: FormGroup = new FormGroup({});
+  errorMessageForCurrent:string = "";
+  errorMessageForNew: string = "";
+  errorMessageForConfirm:string = "";
+  successMessage:string = "";
+  passwordValueChangesSubscription: Subscription = new Subscription();
+
+  private userService = inject(UserService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    this.passwordForm = this.formBuilder.group({
+      currentPassword: ['', Validators.required],
+      newPassword: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+    this.passwordValueChangesSubscription = this.passwordForm.get('currentPassword')!.valueChanges.subscribe(() => {
+      this.errorMessageForCurrent = '';
+    });
+    this.passwordValueChangesSubscription = this.passwordForm.get('newPassword')!.valueChanges.subscribe(() => {
+      this.errorMessageForNew = '';
+    });
+    this.passwordValueChangesSubscription = this.passwordForm.get('confirmPassword')!.valueChanges.subscribe(() => {
+      this.errorMessageForConfirm = '';
+    });
+  }
+
+  onSubmit():void{
+    if(this.passwordForm.value){
+      const currentPassword = this.passwordForm.get('currentPassword')?.value;
+      const newPassword = this.passwordForm.get('newPassword')?.value;
+      const confirmPassword = this.passwordForm.get('confirmPassword')?.value;
+
+      if (newPassword !== confirmPassword) {
+        this.errorMessageForConfirm = "Passwords do not match";
+        return;
+      }
+
+      if (newPassword == currentPassword) {
+        this.errorMessageForNew = "Current Password and New Password are same";
+        return;
+      }
+
+      this.userService.updatePassword(currentPassword, newPassword).subscribe({
+        next: (response: any) => {
+          this.successMessage = response.message;
+          setTimeout(() => {
+            this.router.navigate(['/user/user-details']);
+          }, 2000);
+        },
+        error: (error: any) => {
+          this.errorMessageForCurrent = error.error.message;
+        }
+      });
+    }
+    else{
+      this.makeFormDirty(this.passwordForm);
+    }
+  }
+
+  makeFormDirty(form:FormGroup){
+    Object.keys(form.controls).forEach(key => {
+      form.controls[key].markAsDirty();
+      form.controls[key].markAsTouched();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.passwordValueChangesSubscription.unsubscribe();
+  }
 }
